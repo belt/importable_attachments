f24f734f81a13df1a6c31e7f459735888fa1e503
Paul Belt <saprb@channing.harvard.edu>
2013-04-04 22:35:21 -0400

    propper spec for version

--- spec/models/importable_attachments/version_spec.rb
+++ spec/models/importable_attachments/version_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
 
-module ImportableAttachments
-  describe Version do
-    pending "add some examples to (or delete) #{__FILE__}"
+describe ImportableAttachments::Version do
+  # :call-seq:
+  # mock_version [:opts]
+  #
+  # yields a mock Version object
+
+  def mock_version(opts = {})
+    lopts = {:id => 27, :item_type => nil, :item_id => nil}
+    @version = mock_model(Version, lopts.merge(opts))
+    @version
+  end
+
+  before :each do
+    mock_version
+  end
+
+  context 'validations' do
+    it { should have_valid(:item_id).when(nil, 1, 'uniqueId', 'unique_id', 'unique.id') }
+
+    it { should have_valid(:item_type).when(nil, 'Attachment', 'ImportableAttachment::Attachment') }
+    it { should_not have_valid(:item_type).when("Test \x0 data") }
   end
 end
