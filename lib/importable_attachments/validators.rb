228dc0fd2eb7c1cefbd3421c674fd393c3c8d63a
Paul Belt <saprb@channing.harvard.edu>
2013-04-12 04:13:08 -0400

    enginize validation

--- lib/importable_attachments/validators.rb
+++
@@ -1,7 +0,0 @@
-module ImportableAttachments # :nodoc:
-  module Validators # :nodoc:
-  end
-end
-
-require 'importable_attachments/validators/csv'
-require 'importable_attachments/validators/excel'
