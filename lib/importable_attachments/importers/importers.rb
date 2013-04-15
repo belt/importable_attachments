fc382380ddb5292405aa760b2731190db57abf96
Paul Belt <saprb@channing.harvard.edu>
2013-04-15 08:08:25 -0400

    remove extraneous file

--- lib/importable_attachments/importers/importers.rb
+++
@@ -1,8 +0,0 @@
-module ImportableAttachments::Importers # :nodoc:
-end
-
-require 'importable_attachments/importers/csv'
-require 'importable_attachments/validators/csv'
-require 'importable_attachments/importers/excel'
-require 'importable_attachments/validators/excel'
-
