5918109febe15adf16d1f38ef2b27f00b0e3a855
Paul Belt <saprb@channing.harvard.edu>
2013-06-06 17:15:45 -0400

    comment

--- lib/importable_attachments/engine.rb
+++ lib/importable_attachments/engine.rb
@@ -2,6 +2,7 @@ require 'configuration'
 require 'rails/alpha_numeric_validator'
 
 module ImportableAttachments
+  # integrate importable_attachments machina into rails
   class Engine < ::Rails::Engine
     isolate_namespace ImportableAttachments
   end
